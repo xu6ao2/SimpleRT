9e0d102fe554d71c453f20f2d0b90e500b1cc740
xu6ao2 <xu6ao2@outlook.com>
2024-12-08 22:32:03 -0800

    Update resolv_compat.h

--- simple-rt-cli/include/resolv_compat.h
+++ simple-rt-cli/include/resolv_compat.h
@@ -6,7 +6,6 @@
  * Note: res_init() is actually deprecated according to
  * http://docs.oracle.com/cd/E36784_01/html/E36875/res-nclose-3resolv.html
  **************************************************************************/
-#include <string.h>
 
 static inline int res_ninit(res_state statp)
 {
